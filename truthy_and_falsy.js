fb2b2c674880a04089dd4404e027c422f4c3c8b5
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-17 14:53:49 +0100

    Update truthy_and_falsy.js

--- truthy_and_falsy.js
+++ truthy_and_falsy.js
@@ -6,4 +6,10 @@ if (wordCount) {
   console.log("Better get to work!");
 }
 
+let favouritePhrase = "";
 
+if (favouritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log("This string is definitely empty.");
+}
