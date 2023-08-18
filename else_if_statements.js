93e95413b47ad3020b830a1655d3c2192240afc2
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-18 11:00:28 +0100

    Update else_if_statements.js

--- else_if_statements.js
+++ else_if_statements.js
@@ -2,6 +2,12 @@ let season = 'summer';
 
 if (season === 'spring') {
   console.log('It\s spring! The trees are budding!');
-} else {
+} else if (season === 'winter') {
+  console.log('It\s winter! Everything is covered in snow.');
+} else if (season === 'autumn') {
+  console.log('It\s autumn! The leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\s sunny and warm because it\s summer!');
+
   console.log('Invalid season.');
 }
