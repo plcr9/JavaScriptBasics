cbf95e5a4159f93f7159d309a215e7d2c3019c1a
Paul <118978940+plcr9@users.noreply.github.com>
2023-08-18 10:54:39 +0100

    Update ternary_operator.js

--- ternary_operator.js
+++ ternary_operator.js
@@ -1,1 +1,11 @@
+let isLocked = false;
 
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect');
+
+let favouritePhrase = 'Love That!';
+
+favouritePhrase === 'Love That!' ? console.log('I love that!') : console.log('I dont love that!");
